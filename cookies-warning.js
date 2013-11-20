fc2a5e002568eccf7f70cde8d91b297133d3ae4d
Carlos Cabo <carloscabo@gmail.com>
2013-11-20 21:04:30 +0100

    Pure JS

--- cookies-warning.js
+++ cookies-warning.js
@@ -14,29 +14,39 @@
   },
 
   appendWarning: function(lang) {
-    var t = '';
+    var
+      t = '',
+      h = '',
+      d = document.createElement('div'),
+      body = document.getElementsByTagName('body')[0];
+
     // If no lang set to 'es'
     if (lang === undefined) {
       lang = 'es';
     }
+
     // If passing long string
     if (lang.length > 10) {
       t = lang;
     } else {
       t = CW.Locales[lang];
     }
-    // Write warning
-    $('<div id="cookies-warning"><p><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px"><circle fill="'+CW.svg_bg+'" cx="16" cy="16" r="15" class="bg" /><circle fill="'+CW.svg_fg+'" cx="16" cy="7.216" r="2.733" class="fg"/><rect  fill="'+CW.svg_fg+'" x="13.267" y="12.417" width="5.466" height="14.938" class="fg"/></svg>'+t+'</p></div>').appendTo('body');
+
+    // Create MSG
+    h = '<p><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px"><circle fill="'+CW.svg_bg+'" cx="16" cy="16" r="15" class="bg" /><circle fill="'+CW.svg_fg+'" cx="16" cy="7.216" r="2.733" class="fg"/><rect  fill="'+CW.svg_fg+'" x="13.267" y="12.417" width="5.466" height="14.938" class="fg"/></svg>'+t+'</p>';
+    d.setAttribute('id', 'cookies-warning');
+    d.innerHTML =  h;
+
+    // Hide when clicked
+    d.onclick = function () {
+      this.parentElement.removeChild(this);
+    };
+
+    // Append
+    body.appendChild(d);
+
     // Set cookie
     CW.set(CW.cookie_name, true, 999);
-    // Hide when click OLDER JQuery
-    $('#cookies-warning').click(function(e) {
-      $(this).remove();
-    });
-    // Hide when click newer JQuery
-    /*$('#cookies-warning').on('click', function(e){
-      $(this).remove();
-    });*/
   },
 
   // Cookies handle
@@ -76,6 +86,15 @@
     } else {
       return false;
     }
+  },
+
+  // DELETE cookie
+  // Simple set expiring to 0 :-)
+  del: function(c_name) {
+    if (c_name === undefined) {
+      c_name = CW.cookie_name;
+    }
+    CW.set(c_name, '', 0);
   }
 };
 
