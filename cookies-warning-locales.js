8274502e1263b6ec25d945ad558968e34a49016e
carloscabo <carloscabo@the-cocktail.com>
2014-01-14 15:45:15 +0100

    Añadido stilo alternativo

--- cookies-warning-locales.js
+++ cookies-warning-locales.js
@@ -1,5 +1,5 @@
 CW.Locales = {
-  es: '<b>Este Portal Web hace uso de cookies.</b> Si acepta expresamente nuestra <a href="/es/politica-cookies" id="cookies-warning-more-info">política de cookies</a> en la fase de registro y/o continua en la Web navegando por distintas páginas, utilizando la barra de desplazamiento, o accediendo a diferentes áreas y menús, de esa acción se inferirá que acepta la instalación y uso de las cookies. <a href="#" id="cookies-warning-close">Cerrar</a>'
+  es: '<b>Este Portal Web hace uso de cookies.</b><br>Si continua en nuestra web navegando por distintas áreas, secciones y menús o cierra esta advertencia se inferirá que acepta la instalación y uso de las cookies en nuestro sitio. Puede consultar la <a href="/es/politica-cookies" id="cookies-warning-more-info">información sobre las cookies que empleamos</a>. <a href="#" id="cookies-warning-close">Aceptar</a>'
   ,
   en: '<b>Cookies\' Warning. </b>We use first and third-party cookies to enhance your experience and provide better service.<br>By continuing to browse, it is assumed that you accept the use of cookies on our site. Click here to learn <a href="/en/cookie-policy">how to modify your settings</a>.'
 };
